f7c62243c0329d6b2ef3752f0fdde0949991c6b1
Chet Zhang <60759433+carlisle0615@users.noreply.github.com>
2021-04-05 16:11:54 +0800

    Constants and Unified Return Result

---
+++ src/main/java/com/csse433/blackboard/common/Constants.java
@@ -0,0 +1,6 @@
+package com.csse433.blackboard.common;
+
+public class Constants {
+
+
+}
---
+++ src/main/java/com/csse433/blackboard/common/Result.java
@@ -0,0 +1,97 @@
+package com.csse433.blackboard.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author zhangx8
+ */
+@Data
+public class Result implements Serializable {
+
+    /**
+     * Success status code
+     */
+    private static final String SUCCESS_CODE = "0";
+
+    /**
+     * Error status code
+     */
+    private static final String ERROR_CODE = "-1";
+
+    /**
+     * Error default message
+     */
+    private static final String ERROR = "error";
+
+    /**
+     * Success default message
+     */
+    private static final String OK = "ok";
+
+    /**
+     * Status Code
+     */
+    private String code;
+
+    /**
+     * Message
+     */
+    private String msg;
+
+    /**
+     * Data
+     */
+    private Object data;
+
+    public static Result fail() {
+        return makeErrorResult(null, ERROR);
+    }
+
+    public static Result fail(Object o) {
+        return makeErrorResult(o, ERROR);
+    }
+
+    public static Result fail(String message) {
+        return makeErrorResult(null, message);
+    }
+
+    public static Result fail(Object o, String message) {
+        return makeErrorResult(o, message);
+    }
+
+    private static Result makeErrorResult(Object t, String message) {
+        return makeResult(t, message, ERROR_CODE);
+    }
+
+    public static Result success() {
+        return makeSuccessResult(null, OK);
+    }
+
+    public static Result success(Object o) {
+        return makeSuccessResult(o, OK);
+    }
+
+    public static Result success(String message) {
+        return makeSuccessResult(null, message);
+    }
+
+    public static Result success(Object o, String message) {
+        return makeSuccessResult(o, message);
+    }
+
+    private static Result makeSuccessResult(Object t, String message) {
+        return makeResult(t, message, SUCCESS_CODE);
+    }
+
+    private static Result makeResult(Object t, String message, String code) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setData(t);
+        result.setMsg(message);
+        return result;
+    }
+
+
+}
