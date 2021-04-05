9f0e8eebdc9bca53fcf42ee87dced8bffb6180a1
Chet Zhang <60759433+carlisle0615@users.noreply.github.com>
2021-04-05 12:41:35 +0800

    First commit

---
+++ src/main/java/com/csse433/blackboard/BlackboardApplication.java
@@ -0,0 +1,13 @@
+package com.csse433.blackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csse433/blackboard/BlackboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csse433.blackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
