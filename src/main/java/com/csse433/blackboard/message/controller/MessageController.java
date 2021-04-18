f4eeaf4689e29818b75e013358940fd29f56ffb1
Chet Zhang <60759433+carlisle0615@users.noreply.github.com>
2021-04-18 20:36:51 +0800

    restructure message controller

--- src/main/java/com/csse433/blackboard/message/controller/MessageController.java
+++ src/main/java/com/csse433/blackboard/message/controller/MessageController.java
@@ -2,18 +2,13 @@ package com.csse433.blackboard.message.controller;
 
 import com.csse433.blackboard.auth.dto.UserAccountDto;
 import com.csse433.blackboard.auth.service.AuthService;
-import com.csse433.blackboard.message.dto.MessageDto;
+import com.csse433.blackboard.message.dto.InboundMessageDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
 
-import javax.mail.Message;
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 
 /**
  * @author chetzhang
@@ -29,25 +24,25 @@ public class MessageController {
     private AuthService authService;
 
     @MessageMapping("/toUser")
-    public void toUser(MessageDto messageDto) {
-        log.info(messageDto.toString());
-        UserAccountDto user = authService.findUserByToken(messageDto.getToken());
+    public void toUser(InboundMessageDto inboundMessageDto) {
+        log.info(inboundMessageDto.toString());
+        UserAccountDto user = authService.findUserByToken(inboundMessageDto.getToken());
         if (user == null) {
             log.info("token is invalid");
             return;
         }
         String usernameFromToken = user.getUsername();
-        if (!messageDto.getFrom().equals(usernameFromToken)) {
+        if (!inboundMessageDto.getFrom().equals(usernameFromToken)) {
             log.info("Token does not correspond to current user.");
             return;
         }
-        messagingTemplate.convertAndSendToUser(messageDto.getTo(), "/response", messageDto.getContent());
+        messagingTemplate.convertAndSendToUser(inboundMessageDto.getTo(), "/response", inboundMessageDto.getContent());
     }
 
     @MessageMapping("/toGroup")
-    public void toGroup(MessageDto messageDto) {
-        log.info(messageDto.toString());
-        messagingTemplate.convertAndSendToUser(messageDto.getTo(), "/response", messageDto.getContent());
+    public void toGroup(InboundMessageDto inboundMessageDto) {
+        log.info(inboundMessageDto.toString());
+        messagingTemplate.convertAndSendToUser(inboundMessageDto.getTo(), "/response", inboundMessageDto.getContent());
         // to: groupid or sth identifies group
     }
 
