ffa2773b42d74c0e74349fa499d01f24797c7ef7
Chet Zhang <carlisle0615@gmail.com>
2021-04-18 20:38:34 +0800

    toGroup demo

--- src/main/java/com/csse433/blackboard/message/controller/MessageController.java
+++ src/main/java/com/csse433/blackboard/message/controller/MessageController.java
@@ -1,8 +1,11 @@
 package com.csse433.blackboard.message.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.csse433.blackboard.auth.dto.UserAccountDto;
 import com.csse433.blackboard.auth.service.AuthService;
 import com.csse433.blackboard.message.dto.MessageDto;
+import com.csse433.blackboard.util.TokenUtil;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -14,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 /**
  * @author chetzhang
@@ -31,6 +36,8 @@ public class MessageController {
     @MessageMapping("/toUser")
     public void toUser(MessageDto messageDto) {
         log.info(messageDto.toString());
+
+
         UserAccountDto user = authService.findUserByToken(messageDto.getToken());
         if (user == null) {
             log.info("token is invalid");
@@ -41,12 +48,23 @@ public class MessageController {
             log.info("Token does not correspond to current user.");
             return;
         }
+        authService.extendExpireTime(messageDto.getToken());
         messagingTemplate.convertAndSendToUser(messageDto.getTo(), "/response", messageDto.getContent());
     }
 
     @MessageMapping("/toGroup")
     public void toGroup(MessageDto messageDto) {
         log.info(messageDto.toString());
+//        String to = messageDto.getTo();
+//        Group g = GroupService.getGroupFromId(to);
+//        List<UserAccountDto> userList = g.getUserList();
+//        for (UserAccountDto userAccountDto : userList) {
+//            String token = TokenUtil.getTokenFromUser(userAccountDto.getUsername());
+//        }
+//
+//        String s = JSON.toJSONString(new Object());
+
+
         messagingTemplate.convertAndSendToUser(messageDto.getTo(), "/response", messageDto.getContent());
         // to: groupid or sth identifies group
     }
