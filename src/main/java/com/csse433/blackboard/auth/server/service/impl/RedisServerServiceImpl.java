0b72a943c5f7da54818a3408d387560416e24d54
Chet Zhang <carlisle0615@gmail.com>
2021-05-12 20:22:41 +0800

    redis disconnection

--- src/main/java/com/csse433/blackboard/auth/server/service/impl/RedisServerServiceImpl.java
+++ src/main/java/com/csse433/blackboard/auth/server/service/impl/RedisServerServiceImpl.java
@@ -2,9 +2,12 @@ package com.csse433.blackboard.auth.server.service.impl;
 
 import com.csse433.blackboard.auth.server.service.RedisServerService;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.protocol.ConnectionWatchdog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.connection.RedisClusterConnection;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -15,6 +18,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.types.RedisClientInfo;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Slf4j
 public class RedisServerServiceImpl implements RedisServerService {
@@ -24,15 +30,39 @@ public class RedisServerServiceImpl implements RedisServerService {
 
     private static volatile boolean isConnected;
 
+    private RedisConnectionFactory requiredConnectionFactory;
+
+    @Value("${application.config.redis-heart-beat}")
+    private int heartbeatInterval;
+
+    {
+        new Thread(() -> {
+            while (true){
+                if(isConnected){
+                    log.info("Redis Heart Beat Check: Connected.");
+                } else {
+                    log.error("Redis Heart Beat Check: Reconnecting.");
+                }
+                try {
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(heartbeatInterval));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    redisTemplate.keys("");
+                    isConnected = true;
+                } catch (Exception e) {
+                    isConnected = false;
+                }
+            }
+        }).start();
+    }
+
+    @Autowired
+    private RedisAutoConfiguration redisAutoConfiguration;
     @Override
     public boolean isConnected() {
-//        try {
-//            redisTemplate.getRequiredConnectionFactory().getConnection();
-//            return true;
-//        } catch (RedisConnectionFailureException e) {
-//            e.printStackTrace();
-//            return false;
-//        }
         return isConnected;
     }
 
--- src/test/java/com/csse433/blackboard/BlackboardApplicationTests.java
+++ src/test/java/com/csse433/blackboard/BlackboardApplicationTests.java
@@ -2,21 +2,54 @@ package com.csse433.blackboard;
 
 import com.csse433.blackboard.common.RelationTypeEnum;
 import com.csse433.blackboard.friend.dao.FriendDao;
+import io.lettuce.core.RedisClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import reactor.core.publisher.Mono;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @SpringBootTest
 class BlackboardApplicationTests {
 
 
     @Autowired
-    private FriendDao friendDao;
+    private RedisTemplate redisTemplate;
+
+
+    @Autowired
+    private
+
 
     @Test
     void contextLoads() {
-        RelationTypeEnum userRelation = friendDao.findUserRelation("zhangx8", "chaiq");
-        System.out.println(userRelation.toString());
+
+
+        try {
+            redisTemplate.getRequiredConnectionFactory().getConnection();
+
+            System.out.println(true);
+        } catch (RedisConnectionFailureException e){
+            System.out.println(false);
+        }
+
+
+    }
+
+    @Test
+    void test2(){
+        while(true){
+            boolean existingConnection = TransactionSynchronizationManager.hasResource(redisTemplate.getRequiredConnectionFactory());
+            System.out.println(existingConnection);
+        }
     }
 
 }
